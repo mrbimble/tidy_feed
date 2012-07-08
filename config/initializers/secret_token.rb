fde24b82c1bdaa16f1acefeddb657ee1c5b9e5a1
Nick Schneider <schn0573@comcast.net>
2012-07-07 22:34:43 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TidyFeed::Application.config.secret_token = 'c0ede3d674d8c5a569258458719ccdd00572ec7f1641c7d6a021743e9a63250745742d7b7457359707b2f2af70cb96238d03d8452af64e97cec0874e70462fad'
