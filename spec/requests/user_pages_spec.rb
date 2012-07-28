22893b89999976f8b8230e592dbcfe116c59ce81
Nick Schneider <schn0573@comcast.net>
2012-07-28 08:50:23 -0500

    Updated user pages tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,47 +6,41 @@ describe 'User Pages' do
 
 	describe "index" do
 		let(:user) { FactoryGirl.create(:user) }
+		let(:admin) { FactoryGirl.create(:admin) }
 
 		before(:all) { 30.times { FactoryGirl.create(:user) } }
 		after(:all) { User.delete_all }
 
-		before(:each) do
-			sign_in user
-			visit users_path
-		end
+		describe "as an admin user" do
+			before do
+				sign_in admin
+				visit users_path
+			end
 
-		it { should have_selector('title', text: 'All Users') }
-		it { should have_selector('h1', text: 'All Users') }
+			it { should have_selector('title', text: 'All Users') }
+			it { should have_selector('h1', text: 'All Users') }
 
-		describe "pagination" do
-			it { should have_selector('div.pagination') }
 
-			it "should list each user" do
-				User.paginate(page: 1).each do |user|
-					page.should have_selector('li', text: user.name)
+			describe "pagination" do
+				it { should have_selector('div.pagination') }
+
+				it "should list each user" do
+					User.paginate(page: 1).each do |user|
+						page.should have_selector('li', text: user.name)
+					end
 				end
 			end
-		end
 
-		describe "delete links" do
-
-			it { should_not have_link('Delete') }
-
-			describe "as an admin user" do
-				let(:admin) {FactoryGirl.create(:admin) }
-				before do
-					sign_in admin
-					visit users_path
-				end
+			describe "delete links" do
 
 				it { should have_link('Delete', href: user_path(User.first)) }
 				it "should be able to delete another user" do
 					expect { click_link('Delete') }.to change(User, :count).by(-1)
 				end
 				it { should_not have_link('Delete', href: user_path(admin)) }
+				
 			end
-		end
-					
+		end					
 	end
 
 	describe "Sign up page" do
@@ -87,7 +81,7 @@ describe 'User Pages' do
 				fill_in "Name", with: "Example User"
 				fill_in "Email", with: "user@example.com"
 				fill_in "Password", with: "foobar"
-				fill_in "Confirmation", with: "foobar"
+				fill_in "Confirm Password", with: "foobar"
 			end
 
 			it "should create a user" do
